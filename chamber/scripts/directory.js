ee97ae2056462f14c4ead323d210d3f0d5691872
landrade0587 <and23002@byui.edu>
2023-10-15 19:06:06 -0500

    added changes directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -3,7 +3,7 @@ dayName = todaysDate.getDate();
 monthName = todaysDate.getMonth() + 1;
 year = todaysDate.getFullYear();
 time = todaysDate.toLocaleTimeString('it-IT');
-document.querySelector('#year').textContent = `${year} Barinas Chamber`;
+document.querySelector('#year').textContent = `${year} Barranquilla Chamber`;
 document.querySelector('#lastModified').textContent = `Last Modification: ${dayName}/${monthName}/${year} ${time}`;
 
 
